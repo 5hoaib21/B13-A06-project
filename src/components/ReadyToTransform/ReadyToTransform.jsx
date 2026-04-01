d6e39a8d13ef004a04486213b231305a5b44e84b
MD ABDUR RAHMAN SHOAIB <mdabdurrahmanshoaib@gmail.com>
2026-04-01 06:20:15 +0600

    fixed some error

--- src/components/ReadyToTransform/ReadyToTransform.jsx
+++ src/components/ReadyToTransform/ReadyToTransform.jsx
@@ -5,17 +5,17 @@ const ReadyToTransform = () => {
   <div className="bg-[#4F39F6] mb-0 mt-18 text-center py-12 sm:py-16 lg:py-20 text-white">
   <div className="space-y-5 max-w-2xl mx-auto px-4 sm:px-6 lg:px-0">
     
-    <!-- Heading -->
+
     <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold">
       Ready to Transform Your Workflow?
     </h1>
 
-    <!-- Paragraph -->
+
     <p className="text-zinc-300 text-sm sm:text-base lg:text-lg">
       Join thousands of professionals who are already using Digitools to work smarter. Start your free trial today.
     </p>
 
-    <!-- Buttons -->
+
     <div className="flex flex-col sm:flex-row justify-center items-center gap-4 sm:gap-5 mt-4">
       <button className="bg-white text-[#4F39F6] px-6 py-2 rounded-full font-semibold w-full sm:w-auto">
         Explore Products
@@ -25,7 +25,7 @@ const ReadyToTransform = () => {
       </button>
     </div>
 
-    <!-- Footer text -->
+
     <p className="text-zinc-300 text-xs sm:text-sm lg:text-base mt-4">
       14-day free trial • No credit card required • Cancel anytime
     </p>
